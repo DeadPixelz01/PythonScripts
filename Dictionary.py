b1a95885b38fd573012571174a5dead3e576ccfa
DeadPixelz01 <deadpixelz01@example.com>
2017-12-31 03:27:14 +1100

    Updated Dictionary.py to v1.2

--- Dictionary.py
+++ Dictionary.py
@@ -7,7 +7,7 @@ from termcolor import colored, cprint
 # Version tracking.
 def version_tracking(v_number):
    cprint("Hi there! Your copy of ADMS is currently on %s"%(v_number), 'magenta')
-version_tracking("v1.0")
+version_tracking("v1.2")
 
 
 # A short introduction to the script.
@@ -16,33 +16,30 @@ cprint("-----------------------------------------------", 'blue')
 cprint("FAQ:", 'blue')
 cprint("----", 'blue')
 cprint("What can this script do?", 'blue')
-print("ADMS lets the user manipulate objects from the script's dictionary.")
+print("ADMS lets the user create and manipulate their very own dictionary.")
 cprint("What can't this script do?", 'blue')
-print("As of v1.0, the script does not allow users to create their own dictionaries.")
+print("As of v1.2, user's can not save and load their custom dictionaries.")
+print("I don't know, play around with it. Since it's free, you can view it's source code and edit it to your liking.")
 cprint("What's the point of this script?!", 'blue')
 print("Over the holidays, I wanted to prove to my colleague that Python was a viable language to teach. \n")
 
 
-# Creating and printing the user's original dictionary.
+# Creating and printing the user's dictionary.
+# Asking the user to name their dictionary.
 usr_dict_name = input('Please name your new dictionary: ')
 
+# A blank dictionary.
 usr_dict = {
-    'Alex' : 1,
-    'Jose' : 2,
-    'Eric' : 3
 }
 
+# Introduces the user to their new dictionary.
 intro_msg = colored('currently looks like this: \n', 'yellow')
 print(usr_dict_name, intro_msg)
 
+# Prints the blank dictionary.
 for x in usr_dict:
     print (x)
 print('\n') 
-
-
-# Defining and asking for user input.
-del_usr_input = input('Please delete a user from {name}: '.format(name = usr_dict_name))
-add_usr_input = input('Please add a user to {name}: '.format(name = usr_dict_name))
 
 
 # True or false messages that inform the user of changes they've made to their dictionary.
@@ -52,33 +49,53 @@ add_usr_pos_msg = colored("has been added to {name}".format(name = usr_dict_name
 add_usr_neg_msg = colored("hasn't been added to {name}".format(name = usr_dict_name), 'red', attrs=['bold'])
 
 
-# Adding/removing objects from the user's dictionary.
-# Deleting del_usr_input from the dictionary.
-if del_usr_input in usr_dict.keys():
-    usr_dict.pop(del_usr_input)
-else:
-    print(del_usr_input, del_usr_neg_msg)
-
-# Adding 'add_usr_input' to the dictionary.
-usr_dict[add_usr_input] = 4
+# User choice.
+# This is new and I have no idea how well it'll work later on down the track...
+# Asking the user for either input 1 or 2 (1 deletes a user, 2 adds a new user).
+while True:
+    usr_choice = input('Please select an operation (1; Delete a user. 2; Add a new user. 3; View dictionary. 4; Exit script.): ')
 
 
-# Checks to see if the objects have been added or deleted.
-if del_usr_input not in usr_dict:
-    print(del_usr_input, del_usr_pos_msg)
-else:
-    print(del_usr_input, del_usr_neg_msg)
+# Deleting a user.
+    if usr_choice == "1":
+        del_usr_input = input('Please delete a user from {name}: '.format(name = usr_dict_name)) 
+        if del_usr_input in usr_dict.keys():
+            usr_dict.pop(del_usr_input)
+        else:
+            print(del_usr_input, del_usr_neg_msg)
+    
+        if del_usr_input not in usr_dict:
+            print(del_usr_input, del_usr_pos_msg, '\n')
+        else:
+            print(del_usr_input, del_usr_neg_msg, '\n')
 
-if add_usr_input in usr_dict:
-    print(add_usr_input, add_usr_pos_msg, '\n')
-else:
-    print(add_usr_input, add_usr_neg_msg, '\n')
+
+# Adding a user.
+    if usr_choice == "2":    
+        add_usr_input = input('Please add a user to {name}: '.format(name = usr_dict_name))
+
+        usr_dict[add_usr_input] = 4
+
+        if add_usr_input in usr_dict:
+            print(add_usr_input, add_usr_pos_msg, '\n')
+        else:
+            print(add_usr_input, add_usr_neg_msg, '\n')
 
 
 # Prints the user's changed dictionary.
-outro_msg = colored('was updated successfully: \n', 'yellow', attrs=['bold'])
-print(usr_dict_name, outro_msg)
-for x in usr_dict:
-    print (x)
-print('\n')
+    if usr_choice == "3":
+        outro_msg = colored('was updated successfully: \n', 'yellow', attrs=['bold'])
+        print(usr_dict_name, outro_msg)
+
+        for x in usr_dict:
+            print (x)
+        print('\n')
+
+
+# Existing the script.
+    if usr_choice == "4":
+        print("Thank you for using ADMS. If you enjoyed the script, support free software like it. Everyone will benefit from it! =)")
+        print("- Alex Smith")
+        time.sleep(1)
+        quit()
 
